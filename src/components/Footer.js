2364a344f45ef280f5d9dbf66c35a5d916e7049f
Efosa Ojomo <76777601+Efzo@users.noreply.github.com>
2023-06-04 01:42:29 +0200

    Feat: Add Dynmaic footer Date year

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,14 @@
 import { Link } from 'react-router-dom';
 export const Footer = () => {
+
+const dateYear = new Date()
+const year = dateYear.getFullYear()
+
   return (
 
           <footer class="bg-white  shadow  dark:bg-gray-800">
               <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-                <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2030 <Link to="/" class="hover:underline">Efzo Viewmate™</Link>. All Rights Reserved.
+                <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© {year} <Link to="/" class="hover:underline">Efzo Viewmate™</Link>. All Rights Reserved.
               </span>
               <ul class="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
                   <li>
